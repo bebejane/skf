45b86bd44a53410d9c47a5397d347e576f187f0c
konst-teknik <information-konst-teknik.se>
2021-10-25 14:32:18 +0200

    Single-newsletter

--- single-newsletter-.php
+++ single-newsletter-.php
@@ -1,26 +1,4 @@
-<?php
-
-// Check rows exists.
-if( have_rows('sections') ):
-
-    // Loop through rows.
-    while( have_rows('sections') ) : the_row();
-
-        // Load sub field value.
-        the_sub_field('headline');
-
-        the_sub_field('text');
-
-        the_sub_field('url');
-    
-        // Do something...
-
-    // End loop.
-    endwhile;
-
-  endif;
-
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html data-editor-version="2" class="sg-campaigns" xmlns="http://www.w3.org/1999/xhtml">
     <head>
       <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
